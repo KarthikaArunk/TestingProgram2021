05c6382217decef0cd5018145f0c9b50757cbc38
karthika <karthika@arunkumar.cloud>
2021-12-16 10:46:00 +1000

    added testing script

--- TestingProgram2021/TestingProgram2021/Program.cs
+++ TestingProgram2021/TestingProgram2021/Program.cs
@@ -1,3 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Committing from Commandline
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestingProgram2021 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //open chrome browser
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            //launch TurnUp portal
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+            //identify username textbox and enter valid username
+            IWebElement usernameTextBox = driver.FindElement(By.Id("UserName"));
+            usernameTextBox.SendKeys("hari");
+
+            //identify the password textbox and enter valid password
+
+            IWebElement passwordTextBox = driver.FindElement(By.Id("Password"));
+            passwordTextBox.SendKeys("123123");
+
+            //click on login button
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+            loginButton.Click();
+
+            //check if user is logged in successfully
+            IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+            if (helloHari.Text == "Hello hari!")
+            {
+                Console.WriteLine("Logged in successfully, Test Passed");
+            }
+            else
+                {
+                Console.WriteLine("Log in failed");
+                }
+        }
+    }
+}
